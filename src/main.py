cc05637df77ec1ae152ff8dba229cdc4fb2ec83b
Jitesh Sahani (JD) <jitesh.sahani@outlook.com>
2025-06-06 02:50:01 +0530

    fix: Update GitHub Actions workflow and main script
    Fix name in GitHub Actions workflow.
    Add demo funtion and test in main script.

---
+++ src/main.py
@@ -0,0 +1,18 @@
+"Main Module for the Application"
+
+
+def main():
+    """
+    Main function to run the application.
+    """
+    print("Welcome to the Application!")
+    return True
+
+
+if __name__ == "__main__":
+    """
+    Entry point for the application.
+    """
+    main()
+    print("Application started successfully.")
+    exit(0)
